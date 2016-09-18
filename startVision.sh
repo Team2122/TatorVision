74889e671eb46e800f4a461ffc0872c721f869bf
Sentient Spud <sentientspud@hotmail.com>
2016-09-18 17:10:20 -0600

    Adjusted exposure time

--- startVision.sh
+++ startVision.sh
@@ -3,7 +3,7 @@
 V4L2_CTL=$(which v4l2-ctl)
 if [ -x "$V4L2_CTL" ]; then
 	# TODO: run proper v4l2 ctl commands
-	$V4L2_CTL -c auto_exposure=1 -c exposure_time_absolute=3
+	$V4L2_CTL -c auto_exposure=1 -c exposure_time_absolute=5
 else
 	echo "v4l2-ctl is not in the path. Not configuring camera"
 fi;
